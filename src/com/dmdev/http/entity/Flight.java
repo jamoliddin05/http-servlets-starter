a162ea7501ca45a20fcbb72f1006c76ad71104f6
jamoliddin05 <jamol05@icloud.com>
2023-12-22 15:16:48 +0500

    Start a practice lesson

---
+++ src/com/dmdev/http/entity/Flight.java
@@ -0,0 +1,14 @@
+package com.dmdev.http.entity;
+
+import java.time.LocalDateTime;
+
+public class Flight {
+    private Long id;
+    private String flightNo;
+    private LocalDateTime departureDate;
+    private String departureAirportCode;
+    private LocalDateTime arrivalDate;
+    private String arrivalAirportCode;
+    private Integer aircraftId;
+    private FlightStatus status;
+}
---
+++ src/com/dmdev/http/servlet/DownloadServlet.java
@@ -0,0 +1,25 @@
+package com.dmdev.http.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/download")
+public class DownloadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Content-Disposition", "attachment; filename=\"filename.txt\"");
+        resp.setContentType("application/json");
+
+        try (var writer = resp.getOutputStream();
+             var stream = DownloadServlet.class.getClassLoader().getResourceAsStream("first.json"))
+        {
+            writer.write(stream.readAllBytes());
+        }
+    }
+}
