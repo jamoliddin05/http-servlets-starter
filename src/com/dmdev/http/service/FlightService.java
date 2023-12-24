2507ff6835d6b76c935dde7115132ef0ea2263a5
jamoliddin05 <jamol05@icloud.com>
2023-12-24 07:29:09 +0500

    Cookies are done

--- src/com/dmdev/http/service/FlightService.java
+++ src/com/dmdev/http/service/FlightService.java
@@ -16,14 +16,14 @@ public class FlightService {
 
     public List<FlightDto> findAll() {
         return flightDao.findAll().stream()
-                .map(flight -> new FlightDto(
-                    flight.getId(),
-                                """
-                                %s - %s - %s
-                        """.formatted(flight.getDepartureAirportCode(),
-                                        flight.getArrivalAirportCode(),
-                                        flight.getStatus())
-                )).collect(toList());
+                .map(flight -> FlightDto.builder()
+                                .id(flight.getId())
+                            .description(
+                            """
+                                     %s - %s - %s
+                                """.formatted(flight.getDepartureAirportCode(), flight.getArrivalAirportCode(), flight.getStatus()))
+                            .build()
+                ).collect(toList());
     }
 
     public static FlightService getInstance() {
--- src/com/dmdev/http/util/ConnectionManager.java
+++ src/com/dmdev/http/util/ConnectionManager.java
@@ -1,10 +1,13 @@
 package com.dmdev.http.util;
 
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public final class ConnectionManager {
+@UtilityClass
+public class ConnectionManager {
     private static final String URL_KEY = "db.url";
     private static final String USER_KEY = "db.user";
     private static final String PASSWORD_KEY = "db.password";
@@ -21,18 +24,12 @@ public final class ConnectionManager {
         }
     }
 
-    private ConnectionManager() {
-    }
-
+    @SneakyThrows
     public static Connection get() {
-        try {
             return DriverManager.getConnection(
                     PropertiesUtil.get(URL_KEY),
                     PropertiesUtil.get(USER_KEY),
                     PropertiesUtil.get(PASSWORD_KEY)
             );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
